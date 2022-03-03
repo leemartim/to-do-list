76614d1cd1524808c9df4f7697d6b5bd16a5fd80
leemartim <nicet.shlee@gmail.com>
2022-03-03 13:30:22 +0900

    객체 / 완료버튼함수 / 랜덤id 생성 함수

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 // +버튼을 클릭하면 할일이 추가된다
 // 삭제 버튼을 누르면 할일이 삭제된다
 // 완료 버튼을 누르면 할일이 끝나면서 밑줄이 간다
+// 1. check 버튼을 클릭하면 true false 전환
+// 2. true 일때 끝난것으로 변화하면서 밑줄
+// 3. false 일때 밑줄 해제
+
 // not done done 탭을 누르면, 언더바가 이동한다
 // done탭은 끝난아이템만, not done탭은 진행중인 아이템만
 // 전체탭을 누르면 전체아이템으로 돌아옴
@@ -11,27 +15,47 @@ let userInput = document.getElementById("user-input");
 let addButton = document.getElementById("add-Button");
 let taskList = []
 
+
 addButton.addEventListener("click", addTask)
 
  
 function addTask(){
-    let taskContent = userInput.value
-    taskList.push(taskContent);
-    console.log(taskList)
+    let task = {
+        id : randomIDGenerate(),
+        taskContent : userInput.value,
+        isComplete : false
+    };
+    taskList.push(task);
+    console.log(taskList);
     render()
 }
 
 function render() {
     let resultHTML = "";
     for (let i = 0; i < taskList.length; i++){
-        resultHTML += `<div class="task-tap-user">
-        <div>${taskList[i]}</div>
+        resultHTML += 
+        `<div class="task-tap-user">
+        <div>${taskList[i].taskContent}</div> 
         <div>
-        <button>완료</button>
+        <button onclick="toggleComplete('${taskList[i].id}')">완료</button>
         <button>삭제</button>
         </div>
-    </div>`
+        </div>`
     }
 
     document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id){
+    for(let i =0; i < taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList[i].isComplete = true
+            break;
+        }
+    }
+    console.log(taskList);
+}
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
